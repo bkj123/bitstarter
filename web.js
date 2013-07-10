b65f1716154c4cdd36a29ba1f66bb528c5aa2df1
Brian Jones <example@gmail.com>
2013-07-10 23:13:18 +0000

    added web.js that points to index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+#!/usr/bin/env node
+
 var express = require('express');
+var fx = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var my_index = fs.readFileSync("index.html");   
+  response.send(my_index.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
